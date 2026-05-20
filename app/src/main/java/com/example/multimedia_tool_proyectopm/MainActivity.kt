c6074be7900659adaaaa1e41ccb1dd73173c9677
EsDsVandal <jesusgg9531@gmail.com>
2026-05-20 14:50:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/multimedia_tool_proyectopm/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.multimedia_tool_proyectopm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.multimedia_tool_proyectopm.ui.theme.Multimedia_Tool_ProyectoPMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Multimedia_Tool_ProyectoPMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Multimedia_Tool_ProyectoPMTheme {
+        Greeting("Android")
+    }
+}
